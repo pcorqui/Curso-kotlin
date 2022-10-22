f0fa6f8b7308dc09c5f1d5693b73147774963042
Paul Cortes <pcorqui@gmail.com>
2022-10-21 23:19:04 -0500

    tipos de datos en git

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,4 +13,17 @@ fun main(args: Array<String>) {
     val nombre = "maria";
 
     println(dinero)
+
+    //tipos de datos en kotlin
+    val boolean = true
+    val numeroLargo = 4L
+    val double: Double = 2.7182
+    val float: Float = 1.1f
+
+    //en kotlin todo es un objeto
+
+    val primerValor = 20
+    val segundoValor = 100
+    val tercerValor = primerValor.minus(segundoValor)
+    print(tercerValor)
 }
