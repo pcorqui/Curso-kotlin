1d0170257abbd25cf6eb2852ade01ec7bfb9afe3
pcorqui <pcorqui@gmail.com>
2022-10-22 16:38:10 -0500

    como usar infix

--- src/main/kotlin/funciones.kt
+++ src/main/kotlin/funciones.kt
@@ -1,21 +1,24 @@
+import kotlin.math.abs
+
 fun main(){
 
-    newTopic("Kotlin")
-    newTopic("Java")
-    newTopic("languages funtionales")
-    newTopic("android")
-
+    newTopic("funciones")
     //println(sum(2,4))
     val a = 5
     val b = 6
     println("$a + $b = ${sum(a,b)}")
     println("$a - $b = ${sub(a,b)}")
     println("$a - $b = ${suboptimizada(a,b)}")
+
+    newTopic("infix")
+    val c = -3
+    println(c.enableAbs(false))
+    println("$a + $c = ${sum(a,c.enableAbs(false))}")
+    println("$a + ${c.enableAbs(true)} = ${sum(a,c.enableAbs(true))}")
 }
 
 fun newTopic(s: String) {
-    println("hello ${s}");
-    sayHello()
+    println("************* ${s} ***************");
 }
 
 //por default las funciones en kotlin son public
@@ -35,3 +38,9 @@ fun sub(a: Int, b: Int): Int{
 }
 
 fun suboptimizada(a:Int, b:Int) = a -b
+
+//funciones infix
+//solo se puede pasar un parametro
+//dentro de kotlin no existe el operador ternario
+//sirven para agregar funciones a la clase
+infix fun Int.enableAbs(enable:Boolean) = if (enable) abs(this) else this
