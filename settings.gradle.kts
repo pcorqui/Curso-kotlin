9155b19da89d88f4cd379ba0b57ffb2d1440292c
Paul Cortes <pcorqui@gmail.com>
2022-10-21 22:59:22 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "Curso-kotlin"
+
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    //var es de lectura y escritural
+    var dinero = 10;
+    dinero = 5
+
+    //val es para constantes
+    val nombre = "maria";
+
+    println(dinero)
+}
