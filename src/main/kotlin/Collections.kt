d0355162f6b3cd008704ed6ae908f4bc300a1fad
pcorqui <pcorqui@gmail.com>
2022-10-22 19:21:26 -0500

    listas mutables

--- src/main/kotlin/Collections.kt
+++ src/main/kotlin/Collections.kt
@@ -1,3 +1,6 @@
+import classes.Group
+import classes.User
+
 fun main(){
     newTopic("Collections")
     newTopic("Solo lectura")
@@ -7,4 +10,29 @@ fun main(){
 
     println(frutaList.get((0..frutaList.size - 1).random()))
     println("index de Uva es ${frutaList.indexOf("Uva")}")
+
+    //arreglos mutables --siempre usar constantes antes que variables en cualquier lenguaje
+    newTopic("mutable list")
+
+    val myUser = User(0,"Paul", "Cortes", Group.Family.ordinal)
+    val bro = myUser.copy(1, "Ivan")
+    val friend = bro.copy(2, group = Group.Friends.ordinal)
+    val userList = mutableListOf(myUser,bro)
+    println(userList)
+    userList.add(friend)
+    println(userList)
+    //userList.removeAt(1)
+    userList.remove(bro)
+    println(userList)
+
+    val userSelectList = mutableListOf<User>()
+    println(userSelectList)
+    //userSelectList.addAll(userList)
+    userSelectList.add(myUser)
+    println("myuser: $userSelectList")
+    userSelectList.set(0,bro)//actualizacion del objeto
+    println(userSelectList)
+    userSelectList.add(myUser) //se permiten los elementos repetidos
+    userSelectList.add(myUser)
+    println(userSelectList)
 }
